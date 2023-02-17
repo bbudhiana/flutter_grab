0ac22a8138707798a101731aaca0474049ac75a3
Bana Budhian <budhiana.bana@gmail.com>
2023-02-17 16:10:39 +0700

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'app.dart';
 import 'config/app_config.dart';
@@ -8,10 +9,17 @@ import 'firebase_options.dart';
 
 void main() async {
   ConfigEnvironment.setEnvironment(Environment.trial);
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  // Keep native splash screen up until app is finished bootstrapping
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   di.init();
   runApp(const MyApp());
+
+  // Remove splash screen when bootstrap is complete
+  FlutterNativeSplash.remove();
 }
